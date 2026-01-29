997daacb7cda87fd8499ab7eaa0bd132ae35998f
harshit-technocrat11 <harshit2z1agrawala@gmail.com>
2026-01-29 12:58:33 +0530

    Done and dusted

--- frontend/voice/tts.py
+++ frontend/voice/tts.py
@@ -1,11 +1,10 @@
 import pyttsx3
 
-engine = pyttsx3.init()
-
-
-engine.setProperty("rate", 180)
-engine.setProperty("volume", 1.0)
-
 def speak(text):
+
+    engine = pyttsx3.init()
+    engine.setProperty("rate", 180)
+    engine.setProperty("volume", 1.0)
+
     engine.say(text)
     engine.runAndWait()
